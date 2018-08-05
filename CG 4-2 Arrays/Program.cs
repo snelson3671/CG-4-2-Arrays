33060833034e99a1cfce05232464b3ba50b65401
Sandra <snelson3671@yahoo.com>
2018-08-05 14:21:28 -0500

    Add project files.

---
+++ CG 4-2 Arrays/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CG_4_2_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
