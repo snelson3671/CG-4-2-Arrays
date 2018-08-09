d9c0dc75cc0874a4cc566ce716024afe79d7b880
Sandra <snelson3671@yahoo.com>
2018-08-09 14:50:32 -0500

    CG 4-2 Arrays

--- CG 4-2 Arrays/Program.cs
+++ CG 4-2 Arrays/Program.cs
@@ -5,8 +5,19 @@ namespace CG_4_2_Arrays
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {   
+            //put the following values into an integer array named numbers
+            int[] numbers = new int[] { 1, 1, 2, 3, 5, 8 };
+
+            //go through each element in the array
+            //beginning with the first one, until you have reached the end 
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                //write each element on the console window
+                Console.WriteLine(numbers[i]);
+            }
+            
+            Console.ReadLine();
         }
     }
 }
